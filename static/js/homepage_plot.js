cdebad3fd3cdc91671421054d70587be4abd50ba
Bismaad Minhas <bismaad@gmail.com>
2021-05-07 15:43:35 -0400

    homepage map modified

--- static/js/homepage_plot.js
+++ static/js/homepage_plot.js
@@ -49,6 +49,7 @@ d3.json("/api/v1.0/censustracts").then((data) => {
         accessToken: API_KEY
     });
 
+
     var grayscalemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
