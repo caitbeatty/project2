399b6d6f87c4b6ded21ad65f878642e8a11a0d85
Bismaad Minhas <bismaad@gmail.com>
2021-05-07 11:21:03 -0400

    Leaflet map showing census tracts

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,2 @@
 // API key
+const API_KEY = "pk.eyJ1IjoiYm1pbmhhcyIsImEiOiJja25wNmlvN3UxYnN4MnVwZTRyZWZuZnB0In0.7GJ5OAsxzdytkkyVed4SZA";
--- static/js/homepage_plot.js
+++ static/js/homepage_plot.js
@@ -5,10 +5,11 @@ d3.json("/api/v1.0/censustracts").then((data) => {
     var geoid = 0
     var curr_arr = []
     var coord_arr = []
+    var coordinates = []
     data.map(row => {
         geoid = parseInt(row[2])
         if(geoid == prev_geoID){
-            curr_arr = [row[6], row[5]]
+            curr_arr = [Number(row[6]), Number(row[5])]
             coord_arr.push(curr_arr)
             prev_geoID = geoid
             coord_dict[geoid] = coord_arr
@@ -16,7 +17,7 @@ d3.json("/api/v1.0/censustracts").then((data) => {
         else{
             coord_arr = []
             prev_geoID = geoid
-            curr_arr = [row[6], row[5]]
+            curr_arr = [Number(row[6]), Number(row[5])]
             coord_arr.push(curr_arr)
             coord_dict[geoid] = coord_arr
         }
@@ -25,42 +26,37 @@ d3.json("/api/v1.0/censustracts").then((data) => {
     // console.log(coord_dict)
 
     Object.entries(coord_dict).forEach(([key, value]) => {
-        console.log(key, value[0][1])
-        // for(i=0; i< value.length; i++){
-        //     console.log(key, value[i][1])
-        // };
+            coordinates.push(value)
     });
 
+    var removed = coordinates.splice(210)
+    console.log(coordinates)
 
+var tracts = new L.LayerGroup()
 
+var grayscalemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
 })
 
-// CODE FOR THE MAP
+var myMap = L.map("map", {
+    center: [40.007797, -75.125260],
+    zoom: 11,
+    layers: [
+        grayscalemap
+    ]
+  });
 
-// var tracts = new L.LayerGroup()
-// var grayscalemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//   tileSize: 512,
-//   maxZoom: 18,
-//   zoomOffset: -1,
-//   id: "mapbox/light-v10",
-//   accessToken: API_KEY
-// })
+  grayscalemap.addTo(myMap)
 
-// var map = L.map("map", {
-//     center: [39.9526, -75.1652],
-//     zoom: 10,
-//     layers: [
-//         grayscalemap
-//     ]
-//   });
+  L.polygon(coordinates, {
+    color: "red",
+    fillcolor: "red",
+    fillOpacity: 0.25
+}).addTo(myMap)
 
-//   grayscalemap.addTo(map)
-
-//   var url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Census_Tracts_2010/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
-//   d3.json(url, function (data){
-//   L.geoJSON(data,{
-//       color: "red"
-//   }).addTo(tracts);
-//   tracts.addTo(map)
-// })
+})
